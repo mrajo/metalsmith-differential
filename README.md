9e00be2ef0268751ef05f73ec2e35de1e2b1a99b
mrajo <anthony.c@stle.co>
2015-10-07 17:04:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# metalsmith-differential
+
+> A Metalsmith plugin for building only changed files
+
+For some reason `metalsmith-changed` wasn't ignoring all of the files I
+expected it to, so I created this for myself.
+
+MIT © [Anthony Castle](http://github.com/mrajo)
---
+++ src/plugin.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var fs = require('fs');
+var each = require('async').each;
+if (!Object.assign) Object.assign = require('object-assign');
+
+function config(params) {
+  var options = {
+    destination: null,
+    force: false
+  };
+
+  if ('object' == typeof params) Object.assign(options, params);
+  if ('string' == typeof string) Object.assign(options, { destination: params });
+
+  return options;
+}
+
+function plugin(params) {
+  var options = config(params);
+
+  return function (files, metalsmith, done) {
+    if (options.force) done();
+
+    var dst = options.destination || metalsmith.destination();
+
+    each(
+      Object.keys(files),
+      function (file, done) {
+        var max_ctime = new Date().getTime();
+        var src_ctime = max_ctime;
+        var dst_ctime = 0;
+
+        if (files[file].stats) {
+        src_ctime = files[file].stats.ctime.getTime();
+        }
+
+        var dst_file = metalsmith.path(dst, file);
+
+        fs.stat(dst_file, function (err, stats) {
+        if (!err) dst_ctime = stats.ctime.getTime();
+
+        if (dst_ctime > src_ctime) {
+          delete files[file];
+        }
+
+        done();
+        });
+      },
+      function (err) {
+        if (err) done(err);
+        done();
+      }
+    );
+  };
+}
+
+module.exports = plugin;
